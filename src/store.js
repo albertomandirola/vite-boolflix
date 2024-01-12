18a86035657941b4ab039da73bb72e4777028aff
albertomandirola <albymandi00@gmail.com>
2024-01-12 19:50:51 +0100

    Milestone 0 done

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+
+    APIKey: '62b98bc472bae2f735c2c119eab112c4',
+    urlFilms: 'https://api.themoviedb.org/3/search/movie?api_key=',
+    urlTv: 'https://api.themoviedb.org/3/search/tv?api_key=',
+
+})
